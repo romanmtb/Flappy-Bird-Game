87d05ea38a2b565b6c2a60a42b5c9feff61867a8
Roman Alexeychenko <roman.alexeychenko@gmail.com>
2018-05-22 16:17:20 +0300

    Add bg, fg, and bird moveup

--- js/game.js
+++ js/game.js
@@ -13,6 +13,40 @@ fg.src = 'img/fg.png';
 pipeUp.src = 'img/pipeUp.png';
 pipeBottom.src = 'img/pipeBottom.png';
 
+// Tap on any button
+document.addEventListener('keydown', moveup);
 
+function moveup() {
+    yPos -= 20;
+}
+
+// Creating blocks
+var pipe = [];
+
+pipe[0] = {
+    x: cvs.width,
+    y: 0
+}
+
+// Bird Positon
+var gap = 90;
+var xPos = 10;
+var yPos = 150;
+var grav = 1;
+
+function draw() {
+    ctx.drawImage(bg, 0, 0);
+    ctx.drawImage(fg, 0, cvs.height - fg.height);
+
+    ctx.drawImage(pipeUp, 100, 0);
+    ctx.drawImage(pipeBottom, 100, 0 + pipeUp.height + gap);
+
+    ctx.drawImage(bird, xPos, yPos);
+
+    yPos +=grav;
+    requestAnimationFrame(draw);
+}
+
+pipeBottom.onload = draw();
 
 
